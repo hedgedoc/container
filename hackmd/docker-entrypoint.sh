dcc0a04e52a86c06d91794034f348978b957f195
Sheogorath <sheogorath@shivering-isles.com>
2017-01-23 05:40:26 +0100

    Add warning for not persistent local data storage

--- hackmd/docker-entrypoint.sh
+++ hackmd/docker-entrypoint.sh
@@ -5,6 +5,24 @@ then
     node_modules/.bin/sequelize db:migrate
 fi
 
+# Print warning if local data storage is used but no volume is mounted
+[ "$HMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
+    echo "
+        ##########################################
+        ###                                    ###
+        ###            !!!WARNING!!!           ###
+        ###                                    ###
+        ###    Using local uploads without     ###
+        ###  persistence is dangerous. You'll  ###
+        ###   loose your data on container     ###
+        ###                removal.            ###
+        ###                                    ###
+        ###            !!!WARNING!!!           ###
+        ###                                    ###
+        ##########################################
+    ";
+} ; }
+
 # wait for db up
 sleep 3
 
