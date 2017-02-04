0648acfe1c42a03002b17a1ddc92c6de2aed9837
Sheogorath <sheogorath@shivering-isles.com>
2017-02-04 09:39:15 +0100

    Add reference

--- hackmd/docker-entrypoint.sh
+++ hackmd/docker-entrypoint.sh
@@ -8,18 +8,19 @@ fi
 # Print warning if local data storage is used but no volume is mounted
 [ "$HMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
     echo "
-        ##########################################
-        ###                                    ###
-        ###            !!!WARNING!!!           ###
-        ###                                    ###
-        ###    Using local uploads without     ###
-        ###  persistence is dangerous. You'll  ###
-        ###   loose your data on container     ###
-        ###                removal.            ###
-        ###                                    ###
-        ###            !!!WARNING!!!           ###
-        ###                                    ###
-        ##########################################
+        #################################################################
+        ###                                                           ###
+        ###                         !!!WARNING!!!                     ###
+        ###                                                           ###
+        ###        Using local uploads without persistence is         ###
+        ###            dangerous. You'll loose your data on           ###
+        ###              container removal. Check out:                ###
+        ###  https://docs.docker.com/engine/tutorials/dockervolumes/  ###
+        ###                                                           ###
+        ###                          !!!WARNING!!!                    ###
+        ###                                                           ###
+        ##################################################################
+        
     ";
 } ; }
 
