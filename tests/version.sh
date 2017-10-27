a9a4401dcf1c8c0c8bb4f5cc68f0d5f77f8f7606
Sheogorath <sheogorath@shivering-isles.com>
2017-10-27 22:33:10 +0200

    Update to node version 6.11

--- tests/version.sh
+++ tests/version.sh
@@ -18,7 +18,7 @@ command_exits() { command -v $1 >/dev/null 2>&1 || { echo >&2 "I require $1 but 
 
 # Docker latest version tag
 # See: https://www.shivering-isles.com/helpful-shell-snippets-for-docker-testing-and-bootstrapping/
-docker_image_latest_tag() { REPO=$1; [ $(echo $REPO | grep '/' | wc -l) -eq 0 ] && REPO=library/$1; wget -q -O- https://registry.hub.docker.com/v2/repositories/${REPO}/tags | jq .results[].name | sort -V | sed -e 's/"//g' | grep -P '[[:digit:]]+((.[[:digit:]]+)?.[[:digit:]]+)' | tail -1; }
+docker_image_latest_tag() { REPO=$1; [ $(echo $REPO | grep '/' | wc -l) -eq 0 ] && REPO=library/$1; wget -q -O- https://registry.hub.docker.com/v2/repositories/${REPO}/tags\?page\=1\&page_size\=250 | jq .results[].name | sort -V | sed -e 's/"//g' | grep -P "^6" | grep -P '^[[:digit:]]+((.[[:digit:]]+)?.[[:digit:]]+$)' | tail -1; }
 
 # Version comparison greater or equal
 # See: https://www.shivering-isles.com/helpful-shell-snippets-for-docker-testing-and-bootstrapping/
@@ -34,4 +34,7 @@ docker_base_name() { cat "$1" | grep FROM | sed -e "s/FROM[^[:alpha:]]//g" -e "s
 
 command_exits wget
 
-version_ge $(docker_base_version "$DOCKERFILE") $(docker_image_latest_tag `docker_base_name "$DOCKERFILE"`) && echo "$DOCKERFILE: Base image is up to date! Test successful." || { echo >&2 "$DOCKERFILE: A newer base image is available! Please update."; exit 1; }
+LATEST=$(docker_image_latest_tag `docker_base_name "$DOCKERFILE"`)
+CURRENT=$(docker_base_version "$DOCKERFILE")
+
+version_ge "$CURRENT" "$LATEST" && echo "$DOCKERFILE: Base image is up to date! Test successful." || { echo >&2 "$DOCKERFILE: A newer base image is available! Please updatei to version ${LATEST}."; exit 1; }
