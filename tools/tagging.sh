22fe504b25d55355dc92e2aeb97dcea17c8e8c86
Sheogorath <sheogorath@shivering-isles.com>
2018-01-29 17:02:17 +0100

    Add suffix tagging to all images

--- tools/tagging.sh
+++ tools/tagging.sh
@@ -15,15 +15,20 @@ fi
 PREFIX=${1:-hackmd}
 VERSION=${2}
 SUFFIX=""
-[ "${3}" != "" ] && [ "${3}" != "debian" ] && SUFFIX="-${3}"
+[ "${3}" != "" ] && [ "${3}" != "debian" ] && SUFFIX="${3}"
+SUFFIX_FULL="-${SUFFIX}"
 
 
 
 if [ "$VERSION" != "" ]; then
-    docker tag hackmd:testing "${PREFIX}:$(echo ${VERSION} | cut -d. -f1)${SUFFIX}"
-    docker tag hackmd:testing "${PREFIX}:$(echo ${VERSION} | cut -d. -f1-2)${SUFFIX}"
-    docker tag hackmd:testing "${PREFIX}:$(echo ${VERSION} | cut -d. -f1-3)${SUFFIX}"
-    [ "$SUFFIX" = "" ] && docker tag hackmd:testing "$PREFIX:latest"
+    docker tag hackmd:testing "${PREFIX}:$(echo ${VERSION} | cut -d. -f1)${SUFFIX_FULL}"
+    docker tag hackmd:testing "${PREFIX}:$(echo ${VERSION} | cut -d. -f1-2)${SUFFIX_FULL}"
+    docker tag hackmd:testing "${PREFIX}:$(echo ${VERSION} | cut -d. -f1-3)${SUFFIX_FULL}"
+    if [ "$SUFFIX" = "" ]; then 
+        docker tag hackmd:testing "$PREFIX:latest"
+    else
+        docker tag hackmd:testing "$PREFIX:${SUFFIX}"
+    fi
 else
     echo "No version provided. Skipping tagging..."
 fi
