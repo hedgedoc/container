d2d5326a5325e1937cb7a5c28485f557630c9c5f
Lennart Weller <lennart.weller@hansemerkur.de>
2019-03-29 16:34:53 +0100

    Non-root version of the container for Kubernetes deployments
      - Remove gosu altogether for this build
      - Filesystem permissions should be handled by the storage driver

---
+++ resources/chart-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ "$HMD_DB_URL" != "" ] && [ "$CMD_DB_URL" = "" ]; then
+    CMD_DB_URL="$HMD_DB_URL"
+fi
+
+if [ "$HMD_IMAGE_UPLOAD_TYPE" != "" ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "" ]; then
+    CMD_IMAGE_UPLOAD_TYPE="$HMD_IMAGE_UPLOAD_TYPE"
+fi
+
+if [ "$CMD_DB_URL" = "" ]; then
+    CMD_DB_URL="postgres://hackmd:hackmdpass@hackmdPostgres:5432/hackmd"
+fi
+
+export CMD_DB_URL
+
+DB_SOCKET=$(echo ${CMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
+
+if [ "$DB_SOCKET" != "" ]; then
+    dockerize -wait tcp://${DB_SOCKET} -timeout 30s
+fi
+
+./node_modules/.bin/sequelize db:migrate
+
+# Print warning if local data storage is used but no volume is mounted
+[ "$CMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
+    echo "
+        #################################################################
+        ###                                                           ###
+        ###                        !!!WARNING!!!                      ###
+        ###                                                           ###
+        ###        Using local uploads without persistence is         ###
+        ###            dangerous. You'll loose your data on           ###
+        ###              container removal. Check out:                ###
+        ###  https://docs.docker.com/engine/tutorials/dockervolumes/  ###
+        ###                                                           ###
+        ###                       !!!WARNING!!!                       ###
+        ###                                                           ###
+        #################################################################
+    ";
+} ; }
+
+# run
+exec codimd "$@"
