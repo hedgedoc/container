d2d05c7b4ce595fa172f673302df6ea5a174dfee
oupala <oupala@users.noreply.github.com>
2021-01-28 18:22:51 +0100

    refactor: optimize resources/docker-entrypoint.sh
    Optimize resources/docker-entrypoint.sh following shellcheck and bash best practice.
    
    Signed-off-by: oupala <oupala@users.noreply.github.com>

--- resources/docker-entrypoint.sh
+++ resources/docker-entrypoint.sh
@@ -2,20 +2,20 @@
 
 # Use gosu if the container started with root privileges
 UID="$(id -u)"
-[ "$UID" -eq 0 ] && GOSU="gosu hedgedoc" || GOSU=""
+[ "${UID}" -eq 0 ] && GOSU="gosu hedgedoc" || GOSU=""
 
-if [ "$HMD_DB_URL" != "" ] && [ "$CMD_DB_URL" = "" ]; then
-    CMD_DB_URL="$HMD_DB_URL"
+if [ -n "${HMD_DB_URL}" ] && [ -z "${CMD_DB_URL}" ]; then
+    CMD_DB_URL="${HMD_DB_URL}"
 fi
 
-if [ "$HMD_IMAGE_UPLOAD_TYPE" != "" ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "" ]; then
-    CMD_IMAGE_UPLOAD_TYPE="$HMD_IMAGE_UPLOAD_TYPE"
+if [ -n "${HMD_IMAGE_UPLOAD_TYPE}" ] && [ -z "${CMD_IMAGE_UPLOAD_TYPE}" ]; then
+    CMD_IMAGE_UPLOAD_TYPE="${HMD_IMAGE_UPLOAD_TYPE}"
 fi
 
 DOCKER_SECRET_DB_URL_FILE_PATH="/run/secrets/dbURL"
 
-if [ -f "$DOCKER_SECRET_DB_URL_FILE_PATH" ]; then
-    CMD_DB_URL="$(cat $DOCKER_SECRET_DB_URL_FILE_PATH)"
+if [ -f "${DOCKER_SECRET_DB_URL_FILE_PATH}" ]; then
+    CMD_DB_URL="$(cat ${DOCKER_SECRET_DB_URL_FILE_PATH})"
 fi
 
 # configuration depends on choosen dialect
@@ -39,7 +39,7 @@ if [ -n "${CMD_DB_DIALECT}" ]; then
         CMD_DB_PASSWORD="password"
     fi
     CMD_DB_URL="${CMD_DB_DIALECT}://${CMD_DB_USERNAME}:${CMD_DB_PASSWORD}@${CMD_DB_HOST}:${CMD_DB_PORT}/${CMD_DB_DATABASE}"
-elif [ "$CMD_DB_URL" = "" ]; then
+elif [ -z "${CMD_DB_URL}" ]; then
     CMD_DB_URL="postgres://hedgedoc:password@database:5432/hedgedoc"
 fi
 
@@ -47,15 +47,15 @@ export CMD_DB_URL
 
 DB_SOCKET=$(echo ${CMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
 
-if [ "$DB_SOCKET" != "" ]; then
+if [ "${DB_SOCKET}" != "" ]; then
     dockerize -wait "tcp://${DB_SOCKET}" -timeout 30s
 fi
 
-$GOSU ./node_modules/.bin/sequelize db:migrate
+${GOSU} ./node_modules/.bin/sequelize db:migrate
 
 # Print warning if local data storage is used but no volume is mounted
-[ "$CMD_IMAGE_UPLOAD_TYPE" = "filesystem" ] && { mountpoint -q ./public/uploads || {
-    echo "
+[ "${CMD_IMAGE_UPLOAD_TYPE}" = "filesystem" ] && { mountpoint -q ./public/uploads || {
+    cat <<"EOF"
         #################################################################
         ###                                                           ###
         ###                        !!!WARNING!!!                      ###
@@ -68,16 +68,16 @@ $GOSU ./node_modules/.bin/sequelize db:migrate
         ###                       !!!WARNING!!!                       ###
         ###                                                           ###
         #################################################################
-    ";
+EOF
 } ; }
 
 # Change owner and permission if filesystem backend is used and user has root permissions
-if [ "$UID" -eq 0 ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "filesystem" ]; then
-    if [ "$UID" -eq 0 ]; then
+if [ "${UID}" -eq 0 ] && [ "${CMD_IMAGE_UPLOAD_TYPE}" = "filesystem" ]; then
+    if [ "${UID}" -eq 0 ]; then
         chown -R hedgedoc ./public/uploads
         chmod 700 ./public/uploads
     else
-        echo "
+        cat <<"EOF"
             #################################################################
             ###                                                           ###
             ###                        !!!WARNING!!!                      ###
@@ -90,7 +90,7 @@ if [ "$UID" -eq 0 ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "filesystem" ]; then
             ###                       !!!WARNING!!!                       ###
             ###                                                           ###
             #################################################################
-        ";
+EOF
     fi
 fi
 
@@ -98,4 +98,4 @@ fi
 sleep 3
 
 # run
-exec $GOSU "$@"
+exec ${GOSU} "$@"
