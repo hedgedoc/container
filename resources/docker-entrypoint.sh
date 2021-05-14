2dab7f1899cf0de96749db7b4cb32af683e86681
Sheogorath <sheogorath@shivering-isles.com>
2021-05-14 15:54:54 +0200

    Remove dockerize and entrypoint-script parsing
    Currently the entrypoint script parses the database URL configuration
    string in order to extract the remote location and waits for the
    database to be available.
    
    This patch removes this logic and also removes `dockerize`, which was
    used to wait for the connection. However, this change should not be
    merged without the changes introduced in a commit on the main
    applicaiton, that implements an own connection retry mechanism.
    
    Reference:
    https://github.com/hedgedoc/hedgedoc/pull/1228
    
    Signed-off-by: Sheogorath <sheogorath@shivering-isles.com>

--- resources/docker-entrypoint.sh
+++ resources/docker-entrypoint.sh
@@ -4,30 +4,8 @@
 UID="$(id -u)"
 [ "$UID" -eq 0 ] && GOSU="gosu hedgedoc" || GOSU=""
 
-if [ "$HMD_DB_URL" != "" ] && [ "$CMD_DB_URL" = "" ]; then
-    CMD_DB_URL="$HMD_DB_URL"
-fi
-
 if [ "$HMD_IMAGE_UPLOAD_TYPE" != "" ] && [ "$CMD_IMAGE_UPLOAD_TYPE" = "" ]; then
     CMD_IMAGE_UPLOAD_TYPE="$HMD_IMAGE_UPLOAD_TYPE"
-fi
-
-DOCKER_SECRET_DB_URL_FILE_PATH="/run/secrets/dbURL"
-
-if [ -f "$DOCKER_SECRET_DB_URL_FILE_PATH" ]; then
-    CMD_DB_URL="$(cat $DOCKER_SECRET_DB_URL_FILE_PATH)"
-fi
-
-if [ "$CMD_DB_URL" = "" ]; then
-    CMD_DB_URL="postgres://hedgedoc:password@database:5432/hedgedoc"
-fi
-
-export CMD_DB_URL
-
-DB_SOCKET=$(echo ${CMD_DB_URL} | sed -e 's/.*:\/\//\/\//' -e 's/.*\/\/[^@]*@//' -e 's/\/.*$//')
-
-if [ "$DB_SOCKET" != "" ]; then
-    dockerize -wait "tcp://${DB_SOCKET}" -timeout 30s
 fi
 
 # Print warning if local data storage is used but no volume is mounted
